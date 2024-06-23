4e29c9655eebd7440758dc436bf02051a880bb0b
parkjihye <parkjihye99@naver.com>
2024-06-23 20:29:56 +0900

    Docs : 잘못 올린 파일 수정
     - 전체적으로 변경

--- hm/js/script2.js
+++ hm/js/script2.js
@@ -7,13 +7,4 @@ $(function(){
     $("#nav-bottom > li").on("mouseleave", function(){ 
         $(this).children(".nav-slide").stop().slideUp();
     });
-
-    // 메인배너
-    // (스크롤 내릴 때 배너 크기만큼 보여지게)
-
-    // 카테고리 영역
-    // 무한반복X, 버튼 슬라이드
-
-    // spring 영역
-    // 무한반복, 닷버튼, 버튼 슬라이드
 });
