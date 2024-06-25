3e2221e14b078000d5b685a8a5cf9b17a6f1b52b
parkjihye <parkjihye99@naver.com>
2024-06-25 15:47:27 +0900

    Docs : hm explain page 추가

---
+++ hm/js/script3.js
@@ -0,0 +1,10 @@
+$(function() { 
+    // 내비게이션 영역
+    $("#nav-bottom > li").on("mouseenter", function(){ 
+        $(".nav-slide").stop().slideUp();
+        $(this).children(".nav-slide").stop().slideDown();
+    });
+    $("#nav-bottom > li").on("mouseleave", function(){ 
+        $(this).children(".nav-slide").stop().slideUp();
+    });
+});
